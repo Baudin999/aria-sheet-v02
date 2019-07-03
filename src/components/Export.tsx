c8f35535ebf039a681d7ce8a1345da612db09d68
Baudin <ckelkboom@protonmail.com>
2019-07-03 10:29:20 +0200

    Added an option to update the character JSON manually

--- src/components/Export.tsx
+++ src/components/Export.tsx
@@ -1,13 +1,34 @@
 import * as React from "react";
 import { CharacterContext } from "../services/characterContext";
+import { saveCharacter } from "../redux/actions";
+import { calculateCharacter } from "../services/calculateCharacter";
 
 export const ExportCharacter = () => {
-  let { character } = React.useContext(CharacterContext);
+  const { character } = React.useContext(CharacterContext);
+  if (!character) return null;
+  const [characterJSON, updateCharacter] = React.useState(JSON.stringify(character, null, 4));
+
+  const save = () => {
+    try {
+      let $character = JSON.parse(characterJSON);
+      saveCharacter(calculateCharacter($character));
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <textarea
-      style={{ height: "100%", width: "100%" }}
-      defaultValue={JSON.stringify(character, null, 4)}
-    />
+    <div>
+      <textarea
+        style={{ height: "500px", width: "500px" }}
+        value={characterJSON}
+        onChange={e => updateCharacter(e.target.value)}
+      />
+      <div>
+        <button className="btn btn-warning" onClick={save}>
+          Save {character.name}
+        </button>
+      </div>
+    </div>
   );
 };
